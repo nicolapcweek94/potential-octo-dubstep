3ea322f1b839df5a6feb221d838a08d25819c8a6
nicolapcweek94 <citizenwasp@gmail.com>
2013-10-17 13:55:31 +0200

    Still just a file browser, but with pretty output

--- src/podfm.py
+++ src/podfm.py
@@ -1,24 +1,38 @@
 #!/usr/bin/env python
-import curses, os
+import curses, os, os.path
 
 def main(stdscr):
     path = '/home/wasp/'
+    i = 0
+    offset = 0
+    selected = 0
+    ls = []
+    ldir = []
+    lfile = []
+
     stdscr.clear()
     curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-    i = 0
-    offset = 0
-    selected = 0
- 
+    
+    for f in os.listdir(path):
+        if os.path.isdir(path + f):
+            ldir.append(f)
+        else:
+            lfile.append(f)
+
     while True:
         stdscr.clear()
+        
+        ls = sorted(ldir) + sorted(lfile)
 
-        for f in os.listdir(path)[offset:]:
+        for f in ls[offset:]:
             if i == selected:
                 stdscr.addstr(i, 0, f, curses.A_REVERSE)
-            else:
-                stdscr.addstr(i, 0, f)
+            elif os.path.isdir(path + f):
+                stdscr.addstr(i, 0, f, curses.color_pair(1))
+            elif os.path.isfile(path + f):
+                stdscr.addstr(i, 0, f, curses.color_pair(2))
             i = i + 1
             if i == curses.LINES:
                 break
@@ -35,15 +49,24 @@ def main(stdscr):
         
         elif key == curses.KEY_DOWN:
             if selected == curses.LINES - 1:
-                if offset != len(os.listdir(path)) - curses.LINES:
+                if offset != len(ls) - curses.LINES:
                     offset = offset + 1
-            elif selected < len(os.listdir(path)) - 1:
+            elif selected < len(ls) - 1:
                 selected = selected + 1
         
         elif chr(key) == '\n':
-            path = path + os.listdir(path)[offset + selected] + '/'
+            path = path + ls[offset + selected] + '/'
             selected = 0
             offset = 0
+            ldir = []
+            lfile = []
+
+            for f in os.listdir(path):
+                if os.path.isdir(path + f):
+                    ldir.append(f)
+                else:
+                    lfile.append(f)
+                            
         
         elif key == ord('q'):
             break
